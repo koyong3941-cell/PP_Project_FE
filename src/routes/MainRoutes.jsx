8f1b15a8557981caf9a7029e25e7e1989ff61eff
CU0-0 <bill97720@naver.com>
2026-07-10 14:09:40 +0900

    plantDetail 수정

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,9 +1,5 @@
 import { Route } from "react-router-dom";
 
 import Main from "../main/Main";
-import Main_2 from "../main/Main_3";
 
-export const MainRoutes = [
-  <Route key="main" path="/" element={<Main />} />,
-  <Route key="main2" path="/2" element={<Main_2 />} />,
-];
+export const MainRoutes = [<Route key="main" path="/" element={<Main />} />];
