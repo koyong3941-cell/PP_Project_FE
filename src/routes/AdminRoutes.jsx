e20db251d501f46e7fc3c0cd0c53413fcc052c9e
lovely0503 <minbeom0503@gmail.com>
2026-07-10 10:11:20 +0900

    관리자 수정본

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -4,6 +4,8 @@ import Admin from "../admin/Admin";
 import AdminDash from "../admin/AdminDash";
 import AdminMembers from "../admin/AdminMembers";
 import AdminBoards from "../admin/AdminBoards";
+import AdminBoardData from "../admin/AdminBoardData";
+import AdminBoardDelete from "../admin/AdminBoardDelete";
 import AdminPlants from "../admin/AdminPlants";
 import AdminPlantsPlus from "../admin/AdminPlantsPlus";
 import AdminPlantsEdit from "../admin/AdminPlantsEdit";
@@ -13,9 +15,21 @@ import AdminNoticeEdit from "../admin/AdminNoticeEdit";
 
 export const AdminRoutes = [
   <Route key="admin" path="/admin" element={<Admin />} />,
-  <Route key="admin-dash" path="/admin/dash" element={<AdminDash />} />,
+
   <Route key="admin-member" path="/admin/member" element={<AdminMembers />} />,
+
+  //공지사항 관련
   <Route key="admin-board" path="/admin/board" element={<AdminBoards />} />,
+  <Route
+    key="admin-board-data"
+    path="/admin/board/data"
+    element={<AdminBoardData />}
+  />,
+  <Route
+    key="admin-board-delete"
+    path="/admin/board/delete"
+    element={<AdminBoardDelete />}
+  />,
 
   // 식물 관리
   <Route key="admin-plant" path="/admin/plant" element={<AdminPlants />} />,
@@ -42,4 +56,7 @@ export const AdminRoutes = [
     path="/admin/notice/edit"
     element={<AdminNoticeEdit />}
   />,
+
+  //대시보드
+  <Route key="admin-dash" path="/admin/dash" element={<AdminDash />} />,
 ];
