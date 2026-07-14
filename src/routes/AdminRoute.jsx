0f843557e71ca76118d97580f4fac188dcc27d9d
CU0-0 <bill97720@naver.com>
2026-07-14 16:22:34 +0900

    errorPage 수정

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -4,7 +4,6 @@ const AdminRoute = () => {
   const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
 
-<<<<<<< HEAD
   // 로그인 안 된 경우 → 권한 문제로 ErrorPage 이동
   if (!token) {
     return (
@@ -34,19 +33,6 @@ const AdminRoute = () => {
     );
   }
 
-=======
-  // 로그인 안 된 경우
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
-
-  // ADMIN이 아닌 경우
-  if (role !== "ROLE_ADMIN") {
-    return <Navigate to="/" replace />;
-  }
-
-  // 통과하면 자식 라우트들 렌더링
->>>>>>> main
   return <Outlet />;
 };
 
