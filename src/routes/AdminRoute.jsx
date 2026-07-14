a9a1fbf3111fcb14815aaa098c3be8580448b374
CU0-0 <bill97720@naver.com>
2026-07-14 15:25:29 +0900

    errorPage 수정

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,39 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminRoute = () => {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
+
+  // 로그인 안 된 경우 → 권한 문제로 ErrorPage 이동
+  if (!token) {
+    return (
+      <Navigate
+        to="/error"
+        replace
+        state={{
+          title: "로그인이 필요합니다.",
+          description:
+            "로그인 후 이용 가능한 페이지입니다.\n로그인 후 다시 시도해주세요.",
+        }}
+      />
+    );
+  }
+
+  // ADMIN이 아닌 경우 → 권한 문제로 ErrorPage 이동
+  if (role !== "ROLE_ADMIN") {
+    return (
+      <Navigate
+        to="/error"
+        replace
+        state={{
+          title: "접근 권한이 없습니다.",
+          description: "관리자만 접근할 수 있는 페이지입니다.",
+        }}
+      />
+    );
+  }
+
+  return <Outlet />;
+};
+
+export default AdminRoute;
